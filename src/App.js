a2363184877fac5e5d9e75108b2faf851af42803
Neel Manglani <n.manglani@student.tudelft.nl>
2020-09-24 20:13:41 +0200

    yo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tw from './coolstuff/typewriter/Tw.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
+       
       <header className="App-header">
+      <Tw></Tw>
         <img src={logo} className="App-logo" alt="logo" />
-        <p> This website is currently under development. Check back later for cool content! :)</p>
-        
         
       </header>
 
---
+++ src/coolstuff/typewriter/Tw.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './tw.css';
+
+function Tw() {
+  return (
+    <div>
+        <div class="wrapper">
+         
+  <h1>manglani.nl<span>&nbsp;</span></h1>
+</div>
+    </div>
+    
+  );
+}
+
+export default Tw;
